025624cdf6fe5d8131b416210521c2e6708e0112
Phoebe-S-9 <65313475+Phoebe-S-9@users.noreply.github.com>
2022-04-02 17:19:22 -0400

    Update 190647880_201915560_a04_q1.c
    /*
     * commits:
     * skeleton/frame work
     * semaphores
     * input
     * status,exit,invalid input
     *
     * //maybe RQ and RL? added to thread run...-main issue...
     *
    
     */

--- 190647880_201915560_a04_q1.c
+++ 190647880_201915560_a04_q1.c
@@ -6,29 +6,33 @@
  201915560
  Github: https://github.com/meli1022/CP386-Assignment-4
  ------------------------------
- passing the number of available resources of each type via
- command line to initialize the available array by these values.
- example, if in system there
- were four resource types,
- with ten instances of the first type, f
- ive of the second type,
- seven of the
- third type, and
- eight of the fourth type
- ./Question1 10 5 7 8
- ./190647880_201915560_a04_q1 10 5 7 8
+
  */
 
-//commit 2: mild chagne in  read file //fixed - read command line input //updated matrixes //status ok //(note q = exit) //udpate some inputs --2 lines of input-how get first input?
-//added to thread run //input off //allocated - not calcuated right?
-// safety algorithm// if it leaves the system in a safe state,otherwise will deny it.
-//need a line of input - where input can be 1 word or many ..
-//fgets //getline
-//for now break inptu into 2? //or array input - and check first 2 letters for rq?
-//RQ:
+/*
+ ./190647880_201915560_a04_q1 10 5 7 8
+
+ args: number of available resources of each type
+
+ eg:
+ 4 resource types:
+ 10 instances of the first type, 5 of the second type,
+ 7 of the third type, and 8 of the fourth type
+ */
+
+/*
+ * commits:
+ * skeleton/frame work
+ * semaphores
+ * input
+ * status,exit,invalid input
+ *
+ * //maybe RQ and RL? added to thread run...-main issue...
+ *
+
+ */
+
 //eqns toupdate matrix:	//alloc = input. need = max- alloc[row]? //availbe  = avaible-alloc [cols]?
-//RL
-//
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -36,13 +40,24 @@
 #include<string.h>
 #include <sys/stat.h>
 
-typedef struct customer {
+#include<semaphore.h>
+sem_t sem;
+
+typedef struct customer { //thread
 	int resource[4]; // can have up to 5 resources for the customer
 
-//each thread also need a needed, availble. ?and allocated - of its own?//and id
 } Customer;
 
-int readFile(char *fileName, Customer **customer) {
+typedef struct thread //represents a single thread, you can add more members if required
+{
+	int tid; //char tid[4]; //id of the thread as read from file
+//pthread_t handle;
+//int state;
+	//each thread also need a needed, availble. ?and allocated - of its own?//and id//or just print off from main matrix?
+	//need thread id?
+} Thread;
+
+int readFile(char *fileName, Customer **customer) { //read Maximum resources from file
 
 	FILE *in = fopen(fileName, "r");
 	if (!in) {
@@ -105,291 +120,353 @@ int readFile(char *fileName, Customer **customer) {
 }
 
 void* threadRun(void *t); //the thread function, the code executed by each thread
-//void* safety();//check if safe
-//banker's algorithm.
+//void* threadRun(void *t, int threadNum, int numResourceTypes, int allocated);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { //banker's algorithm.
 
 	//vars
+	sem_init(&sem, 0, 1);	//unnamed
+
+	//n customers for m resource types. //eg 5 customers *4 types
 	Customer *customer = NULL;
+	int numofCustomers = readFile("sample4_in.txt", &customer); //n = 5
+	int numResourceTypes = argc - 1; //m = 4//resources types passed (not including argv[0])
 
-	int numofCustomers = readFile("sample4_in.txt", &customer);
+	int available[numResourceTypes];	//available amount of each resource
 
-	//char *available[100]; //available amount of each resource //
-	int available[3]; //4
+	int maximum[numofCustomers][numResourceTypes]; // maximum demand of each customer //5,4
 
-	for (int i = 1; i < argc; i++) { //not argv[0]
-		available[i] = atoi(argv[i]); //in command line
-	}
+	int allocated[numofCustomers][numResourceTypes]; //amount currently allocated to each customer
+	int need[numofCustomers][numResourceTypes];	//remaining need of each customer
 
-//	for (int i = 1; i < argc; i++) { //not argv[0]
-//		available[i] = argv[i]; //in command line
-//	}
-
-	int maximum[5][4]; // maximum demand of each customer  //[4][5] //argc-1,numcusomters
-	int allocated[5][4]; //amount currently allocated to each customer
-	int need[5][4];	 //remaining need of each customer
-
-	//int safeSequnce[]; //dyanmic?//ad p1 values to it
+	int safeSequnce[100]; //dyanmic?//ad p1 values to it
+	int safeCounter = 0; //num elemes in safe sequnce
 
 	char commands[20]; //RQ. Stastus. etc
-	int p1[20], p2[20], p3[20], p4[20], p5[20];	//p1 - process number . p2 - p5: num resources ?//char
+
+	char p1[20]; //p1 - process number . and  num resources inputed
+	int temp[numResourceTypes];		//array of inputs
 
 	//need to crtate numofCustomers amount of threads...
-	pthread_t t1;	//, t2, t3, t4, t5;
+	//pthread_t t1, t2, t3, t4, t5;
+
 	//output
+
 	printf("Number of Customers: %d\n", numofCustomers);
 
-	printf("Currently Available resources:");//printf("Currently Available resources: 10 5 7 8 \n");
-	for (int i = 1; i < argc; i++) { //not argv[0] //i = 0
-		printf("%d ", available[i]); //in command line
+	//available
+	for (int i = 0; i < numResourceTypes; i++) {
+		available[i] = atoi(argv[i + 1]); //in command line  //not argv[0]
 	}
-//	for (int i = 1; i < argc; i++) { //not argv[0]
-//		printf("%s ", available[i]); //d
-//	}
+
+	printf("Currently Available resources:");
+	for (int i = 0; i < numResourceTypes; i++) {
+		printf("%d ", available[i]);
+	}
 	printf("\n");
+
+	//max
+	for (int i = 0; i < numofCustomers; i++) { //5
+		for (int j = 0; j < numResourceTypes; j++) { //4
+			maximum[i][j] = customer[i].resource[j];
+		}
+	}
 
 	printf("Maximum resources from file:\n"); //contents of file-print line by line
 
-	for (int i = 0; i < numofCustomers; i++) { //5//num customers
-		for (int j = 0; j < argc - 1; j++) { //4 //resources types passed (not including argv[0]
+	for (int i = 0; i < numofCustomers; i++) {
+		for (int j = 0; j < numResourceTypes; j++) {
 
-			maximum[i][j] = customer[i].resource[j];
-			printf("%d ", maximum[i][j]); //printf("%d ", customer[i].resource[j]);
+			printf("%d ", maximum[i][j]);
 		}
 		printf("\n");
 	}
 
-	for (int i = 0; i < numofCustomers; i++) { //5
-		for (int j = 0; j < argc - 1; j++) { //4
+	//allocated and need
+	for (int i = 0; i < numofCustomers; i++) {
+		for (int j = 0; j < numResourceTypes; j++) {
 
 			allocated[i][j] = 0;
 			need[i][j] = 0;
 		}
 	}
 
-	while (1) {
+	while (1) { //program loop
 
 		//input
 		printf("Enter Command: ");
+		scanf("%s", commands);
 
-		scanf("%s", commands);	//multi command? expted 5 input at most?
-		//scanf("%s %d %d %d %d %d",commands, p1, p2, p3, p4, p5); //commands - wrod (RQ). p1- thread number . rest - request nums
-//make rest  null if not rq or rl?
+		if (strcmp(commands, "RQ") == 0) { //command =RQ
+			//	eg RQ 0 1 0 0 1
+			//customer/thread 0 were to request the resources (1, 0, 0, 1)
 
-		//just first part of input mathces?
-		//if (strcmp(commands[0], "RQ") == 0) {
+			/*
+			 * RQ:
+			 * input = process id, requests
+			 * resouces request algo
+			 * if safty algo: allocate. need = max - alloc
+			 * else wati?
+			 */
 
-		//		printf("%c %c", commands[0], commands[1]);
-//		char temp[20];
-//		temp = commands[0] + commands[1];
-//		printf("%d", strcmp(temp, "RQ"));
+			//get other inputs
+			scanf("%s", p1);			//process id
+			int processID = atoi(p1);
+			//printf("process id: %d\n", processID);
 
-		if (strcmp(commands, "RQ") == 0) { //compare
-			//	eg RQ 0 1 0 0 1 //customer/thread 0 were to request the resources (1, 0, 0, 1)
+			for (int i = 0; i < numResourceTypes; i++) { //requested
 
-			//scanf("%d", p1);
-			//printf("%d", allocated[p1][0]);
+				scanf("%s", p1);
+				temp[i] = atoi(p1);
+				//printf("%d ", temp[i]);
+			}
 
-			//allocated[2][0] = p1;
-
-			//int t = &p1;
-			//scanf("%d", &allocated[t][0]);
-
-			//scanf("%d %d %d %d %d", p1, p2, p3, p4, p5);
-
-			//do saftey algo here..
-
-			//saftye algo //to check if this would be safe?
-			int work[5];			//[4]; //temp avaible array
+			// safety algorithm
+			// if it leaves the system in a safe state,otherwise will deny it.
+			//allocate resources if safe
 
 			int isSafe = 1;
-			int finish[5];
 
-			for (int i = 0; i < numofCustomers; i++) { //5
+			//step 1
+			int work[numResourceTypes];	//temp avaible array //m
+			int finish[numofCustomers];	//n
 
+			for (int i = 0; i < numResourceTypes; i++) {
 				work[i] = available[i];
-
 			}
-			for (int i = 0; i < numofCustomers; i++) { //5
+
+			for (int i = 0; i < numofCustomers - 1; i++) {
 				finish[i] = 0; //false
-
 			}
 
-			//find index i
+			//step 2
 
-			for (int i = 0; i < numofCustomers; i++) { //5
+			for (int i = 0; i < numofCustomers; i++) {
 
-				if (finish[i] == 0 && need[i][0] <= work[i]) { //available[i]) { //which need and alloc to compare?
-					printf("yes");
+				if (finish[i] == 0 && need[processID][i] <= work[i]) { //use row of  processID
 
-					work[i] = work[i] + allocated[i][0];
-					//available[i] = available[i] + allocated[i][0];
+				//step 3
+					work[i] = work[i] + allocated[processID][i];
 					finish[i] = 1; //true
 
 				}
 			}
-			for (int i = 0; i < numofCustomers; i++) { //5
-				if (finish[i] == 0) {
+
+			//step 4
+			for (int i = 0; i < numofCustomers; i++) {
+				if (finish[i] == 0) { //not all true -> not safe
 					isSafe = 0; //false
 				}
-
 			}
 
-			if (isSafe == 1) {
-
-				printf("isSafe %d\n", isSafe);
-//do resource alloc
+			//printf("isSafe %d\n", isSafe);
+			if (isSafe == 1) { //resource alloc
 
 				printf("State is safe, and request is satisfied\n");
 
 				//update everything
-				//attmpet at resource alloc
-				for (int i = 1; i < argc; i++) { //not argv[0] //for (int i = 0; i < argc - 1; i++) {
-					//printf("%s ", available[i]); //d
-					available[i] = available[i] - 1; //each input //allcoedcted[j][i]? //last value not right?
-				}
+				safeSequnce[safeCounter] = processID; //add to sequence
+				safeCounter += 1;
 
-				for (int i = 0; i < numofCustomers; i++) { //5
-					for (int j = 0; j < argc - 1; j++) { //4
-						//printf("%d ", need[i][j]);
-						need[i][j] = maximum[i][j] - allocated[i][j];
-					}
+				for (int j = 0; j < numResourceTypes; j++) //avail = avail - request
+					available[j] -= temp[j];
 
-				}
+				for (int j = 0; j < numResourceTypes; j++) //alloc  = alloc +request?
+					allocated[processID][j] += temp[j];
+
+				for (int j = 0; j < numResourceTypes; j++) //need = max - request
+					need[processID][j] = maximum[processID][j] - temp[j];
+				///or need = max - alloc ?
 
 			}
 
-			for (int i = 1; i < argc - 1; i++) { //not argv[0] //for (int i = 0; i < argc - 1; i++) {
-				printf("%d ", available[i]); //d
-				printf("%d ", work[i]);
+		} else if (strcmp(commands, "RL") == 0) {			//command =RL
+			//eg RL 4 1 0 0 0
+			// customer 4 wishes to release the resources (1, 0, 0, 0)
 
-				//work[i] = work[i] - 1; //each input //allcoedcted[j][i]? //last value not right?
+			//get other inputs
+			scanf("%s", p1);			//process id
+			int processID = atoi(p1);
+			//printf("process id: %d\n", processID);
+
+			for (int i = 0; i < numResourceTypes; i++) { //requested
+
+				scanf("%s", p1);
+				temp[i] = atoi(p1);
+				//printf("%d ", temp[i]);
 			}
-			printf("\n");
 
-			//assume safe
-			scanf("%d %d %d %d", &allocated[2][0], &allocated[2][1],
-					&allocated[2][2], &allocated[2][3]); //p1 = 2 //how get p1?
-
-			////if customer_number[i][j]>maximum[i][j]: invalid} {else
-
-			printf("State is safe, and request is satisfied\n");
+			//release//opposite of request? //but assume safe?
 
 			//update everything
 
-//			for (int i = 0; i < numofCustomers; i++) { //5
-//
-//				if (i == p1) { //first num is process number
-//
-//
-//					allocated[i][1] = p2;
-//					allocated[i][2] = p3;
-//					allocated[i][3] = p4;
-//					allocated[i][4] = p5;
-//
-//
-//				}
-//			}
+			for (int j = 0; j < numResourceTypes; j++) //avail
+				available[j] += temp[j]; //-= temp[j];
 
-//			for (int i = 0; i < argc - 1; i++) { //5//numofCustomers
-//				//scanf("%d", &allocated[i][p1]); //
-//				scanf("%d", &allocated[p1][i]);	//scanf("%d", &allocated[p1][i]);
-//			}
+			for (int j = 0; j < numResourceTypes; j++) //alloc
+				allocated[processID][j] -= temp[j]; // += temp[j];
 
-			//------------
+			for (int j = 0; j < numResourceTypes; j++) //need
+				need[processID][j] = maximum[processID][j] + temp[j]; //maximum[processID][j] - temp[j];
 
-		} else if (strcmp(commands, "RL") == 0) {
-			//eg RL 4 1 0 0 0 // customer 4 wishes to release the resources (1, 0, 0, 0),
-			scanf("%d %d %d %d %d", p1, p2, p3, p4, p5); //scanf("%s %s %s %s %s", p1, p2, p3, p4, p5);
 			printf("The resources have been released successfully\n");
 
-		} else if (strcmp(commands, "Status") == 0) {
-			;
+		} else if (strcmp(commands, "Status") == 0) { //command =Status
+
 			/*
-			 * Status command would print all arrays and matrices used
-			 (available, maximum, allocation, and need).
-			 *
+			 *print all arrays and matrices:  (available, maximum, allocation, and need).
 			 */
+
 			printf("Available Resources:\n");
-			for (int i = 1; i < argc; i++) { //not argv[0] //i = 0
-				printf("%d ", available[i]); //in command line
+			for (int i = 0; i < numResourceTypes; i++) {
+				printf("%d ", available[i]);
 			}
 			printf("\n");
 
 			printf("Maximum Resources:\n");
-
 			for (int i = 0; i < numofCustomers; i++) { //5
-				for (int j = 0; j < argc - 1; j++) { //4
-					printf("%d ", maximum[i][j]);
+				for (int j = 0; j < numResourceTypes; j++) { //4
 
+					printf("%d ", maximum[i][j]);
 				}
 				printf("\n");
 			}
 
 			printf("Allocated Resources:\n");
+			for (int i = 0; i < numofCustomers; i++) {
+				for (int j = 0; j < numResourceTypes; j++) {
 
-			for (int i = 0; i < numofCustomers; i++) { //5
-				for (int j = 0; j < argc - 1; j++) { //4
 					printf("%d ", allocated[i][j]);
-
 				}
 				printf("\n");
 			}
 
 			printf("Need Resources:\n");
+			for (int i = 0; i < numofCustomers; i++) {
+				for (int j = 0; j < numResourceTypes; j++) {
 
-			for (int i = 0; i < numofCustomers; i++) { //5
-				for (int j = 0; j < argc - 1; j++) { //4
 					printf("%d ", need[i][j]);
-
 				}
 				printf("\n");
 			}
 
+		} else if (strcmp(commands, "Exit") == 0
+				|| strcmp(commands, "q") == 0) {
+
+			//Exit command is used to exit the loop and the program ////q to debug
+			break;
+
 		} else if (strcmp(commands, "Run") == 0) {
-			;
+
 			/*
 			 Run command executes customers as threads in a safe sequence. Each thread
 			 requests the resources it needs, releases them, and lets the next thread in the sequence run.
 			 */
 
-			//call thred run?
-			printf("Safe Sequence is: 1 3 2 4 0\n"); //need build up values--thes are p1 values
-			//threadRun()
-			//idea- maybe a semphore so thread runs fully?
-			pthread_create(&t1, NULL, &threadRun, NULL);
+			printf("Safe Sequence is: "); //1 3 2 4 0\n"); //need build up values--these are process id values
+			for (int i = 0; i < safeCounter; i++) {
+				printf("%d ", safeSequnce[i]);
+			}
+			printf("\n");
 
-		} else if (strcmp(commands, "Exit") == 0
-				|| strcmp(commands, "q") == 0) { //q to debug
-				/*
-				 *  Exit command is used to exit the loop and the program
-				 */
-			break;
-		} else {			// If user enters a wrong command
+			//call threads in safe sequence order... not sequential...
+			//how to assign thread tid? and other vars? //eg assign thread3 .alloc = row3 of alloc matrix?
+
+			pthread_t t1, t2, t3, t4, t5;
+
+			pthread_create(&t1, NULL, &threadRun, NULL);
+			pthread_create(&t2, NULL, &threadRun, NULL);
+			pthread_create(&t3, NULL, &threadRun, NULL);
+			pthread_create(&t4, NULL, &threadRun, NULL);
+			pthread_create(&t5, NULL, &threadRun, NULL);
+
+			pthread_join(t1, NULL);
+			pthread_join(t2, NULL);
+			pthread_join(t3, NULL);
+			pthread_join(t4, NULL);
+			pthread_join(t5, NULL);
+
+			//&t1.tid = 1;
+
+//			Thread *threads = NULL;
+//			*threads = (Thread*) malloc(sizeof(Thread) * numofCustomers);
+//			for (int k = 0; k < numofCustomers; k++) {
+//
+//				(*threads)[k].tid = k;
+//
+//			}
+//			for (int i = 0; i < numofCustomers; i++) { //5
+//				//&threads[i].tid = i;
+//				pthread_create(&(threads[i].tid), NULL, &threadRun,
+//						&(threads[i])); //create thread
+//
+//				//	pthread_create(&threads[i], NULL, &threadRun, NULL);
+//				//pthread_create(&(threads[i].handle), NULL, threadRun, &threads[i]);
+//			}
+
+		} else { // If user enters a wrong command
 			printf("Invalid input, use one of RQ, RL, Status, Run, Exit\n");
 		}
 	}
-
 	return 0;
+
 }
 
 //Function to run the customer threads
+//void* threadRun(void *t, int threadNum, int numResourceTypes, int allocated) {
 void* threadRun(void *t) {
+	sem_wait(&sem); //semphore so thread runs fully
 
-	printf("\n--> Customer/Thread %d", 1);
-	printf("\nAllocated resources: ");			//1 1 1 1"); //traverse values
-	printf("\nNeeded: "); //3 1 2 1");
-	printf("\nAvailable: "); //4 1 3 3");
+	int numResourceTypes = 4; //how get?
+	int threadNum = 1;
+	//threadNum = ((Thread*) t)->tid;
+	//thread have indiv arrays?//or copy paste to acutally allocate and rlease?
 
+	printf("--> Customer/Thread %d\n", threadNum);
+
+	printf("Allocated resources: ");		//1 1 1 1"); //traverse values
+	for (int j = 0; j < numResourceTypes; j++) { //4
+		printf("%d ", 1); //allocated[threadNum][j]);
+	}
 	printf("\n");
+
+	printf("Needed: "); //3 1 2 1");
+	for (int j = 0; j < numResourceTypes; j++) {
+		printf("%d ", 1); //need[threadNum][j]);
+	}
+	printf("\n");
+
+	printf("Available: "); //4 1 3 3");
+	for (int i = 0; i < numResourceTypes; i++) {
+		printf("%d ", 1); //available[i]);
+	}
+	printf("\n");
+
 	printf("Thread has started\n");
+
+	//update matrix?
+	//availbe = availbe + alloc?
+	for (int i = 0; i < numResourceTypes; i++) {
+		;	//available[i] = available[i] + allocated[threadNum][i];
+	}
+
+//and alloc = 0? //realase all?
+
+	for (int j = 0; j < numResourceTypes; j++) {
+		;	//allocated[threadNum][j] = 0;
+	}
+
 	printf("Thread has finished\n");
 	printf("Thread is releasing resources\n");
 
 //New Available status.
-//avabile = avialbe + alloc?
+
 	printf("New Available: "); //5 2 4 4")
+	for (int i = 0; i < numResourceTypes; i++) {
+		printf("%d ", 1); //available[i]);
+	}
+	printf("\n");
+
+	sem_post(&sem);
 	pthread_exit(0);
 }
-
