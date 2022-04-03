e07888d599cab0cd6612a52d6f5ce687a808fc6c
Phoebe-S-9 <65313475+Phoebe-S-9@users.noreply.github.com>
2022-04-03 19:09:09 -0400

    Update 190647880_201915560_a04_q2.c
    final commit

--- 190647880_201915560_a04_q2.c
+++ 190647880_201915560_a04_q2.c
@@ -20,8 +20,8 @@ typedef struct h {
 	//find
 	int size;
 	int start;
-	int end; //end = start + size
-	int id; //order in memeory?
+	int end;
+
 } Hole;
 
 typedef struct p {
@@ -32,31 +32,29 @@ typedef struct p {
 	//find
 	int start;
 	int end; //end = start + size
-	int id; //order  it was entered- or order of in memeory?
+
 } Process;
 
 int main(int argc, char *argv[]) {
 	int MAX = atoi(argv[1]);
-	//MAX = MAX / 1000; //debug
+
 	Process *processess = NULL;
 	Hole *holes = NULL;
 
 	int numProcesses = 0, numHoles = 1;
 	int totalProcessesSize = 0, totalHolesSize = MAX; //add up all lens
 
-	processess = (Process*) malloc(sizeof(Process) * 20); //* numProcesses); //or just make it big size?
+	processess = (Process*) malloc(sizeof(Process) * 20); //* numProcesses);
 	holes = (Hole*) malloc(sizeof(Hole) * 20); //* numHoles);
 
+	//first hole in memeory- 1 big hole
 	(holes)[numHoles - 1].start = 0;
 	(holes)[numHoles - 1].end = MAX; //- 1;
 	(holes)[numHoles - 1].size = (holes)[0].end - (holes)[0].start;
-	(holes)[numHoles - 1].id = 0; //first hole in memeory
 
 	printf("Allocated %d bytes of memory\n", MAX);
 
-///added: proecces[num processes]? reallco?and remove: processes[process num?]
-
-	char commands[20];
+	char commands[20]; //inputs
 	char name[100];
 	int size;
 	char algo[100];
@@ -78,11 +76,9 @@ int main(int argc, char *argv[]) {
 			 */
 
 			//get other inputs
-			scanf("%s", name); //only if valid..need temp
+			scanf("%s", name);
 			scanf("%d", &size);
 			scanf("%s", algo);
-
-			//printf("(algo %s)\n", algo); //debug
 
 			//best fit algo:================
 			int isFit = 0;
@@ -91,7 +87,8 @@ int main(int argc, char *argv[]) {
 
 			//travesr - find smallest hole size
 			for (int i = 0; i < numHoles; i++) { //numHoles - 1?
-				if ((holes)[i].size < min) {
+
+				if ((holes)[i].size < min && (holes)[i].size >= size) { //is min and fits
 					min = (holes)[i].size;
 					bestHoleIndex = i; //find best hole
 
@@ -102,15 +99,15 @@ int main(int argc, char *argv[]) {
 
 			//printf("bestHoleIndex%d\n", bestHoleIndex); //put the process in here
 
-			if (isFit == 1) {
+			if (isFit == 1) { //if can fit //add to processess
 
-				//added to processess
 				//pname = name. psize = size. pstart = hole start. pend = p start + p size
 
 				strcpy(&(processess)[numProcesses].name, name); //	&(processess)[numProcesses].name = name;
 				(processess)[numProcesses].size = size;
 
 				(processess)[numProcesses].start = holes[bestHoleIndex].start;
+
 				(processess)[numProcesses].end =
 						(processess)[numProcesses].start
 								+ (processess)[numProcesses].size;
@@ -119,16 +116,16 @@ int main(int argc, char *argv[]) {
 						(processess)[numProcesses].size,
 						&(processess)[numProcesses].name);
 
-				//add totla sizes
-
+				//update totla sizes
 				totalProcessesSize += size;
 				totalHolesSize -= size;
 
-				//find if hole was split or combined -> hole count +- 1
-
-				(holes)[bestHoleIndex].size -= size; //h[0]
+//update best hole
+				(holes)[bestHoleIndex].size -= size;
 				(holes)[bestHoleIndex].start = (processess)[numProcesses].end; //hole pushed over. to start where process ends
 
+				//(holes)[bestHoleIndex].end = next process start //this makes last output wrong..
+				// find where next hole start or next process starts? proces[num +1]? //= max or to process[1].start
 				if ((processess)[numProcesses + 1].start > 0) { //if theres a process after. end = start of next process
 
 					(holes)[bestHoleIndex].end =
@@ -137,15 +134,14 @@ int main(int argc, char *argv[]) {
 					(holes)[bestHoleIndex].end = MAX;
 				}
 
-				//need find /next hole start or next process tarts? proces[num +1]? //= max or to process[1].start
-
-				numProcesses += 1;
+				numProcesses += 1; //process was added
 
 			} else {
 				printf("No hole of sufficient size\n");
 			}
 
 		} else if (strcmp(commands, "RL") == 0) {
+
 			/*
 			 * RL <process number/name>:
 			 * RL command will release the memory that has been
@@ -157,28 +153,23 @@ int main(int argc, char *argv[]) {
 			//get other inputs
 			scanf("%s", name);
 
-			printf("releasing memory for process %s\n", name); //"P0");
-			//realse
-			//update process areaay and hole arrays //eg remove form lists?
+			printf("releasing memory for process %s\n", name);
+
+			//update process areaay and hole arrays 
 
 			for (int i = 0; i < numProcesses; i++) {
-				if (strcmp(&(processess)[i].name, name) == 0) { //if found
+				if (strcmp(&(processess)[i].name, name) == 0) { //if found name
 
-					//printf("(found)\n");
-
-					//make a new hole- the exact size of it// if another hole right after it- combine into 1 hole?
+					//make a new hole- the exact size of it
 					numHoles += 1;
 
 					(holes)[numHoles - 1].size = (processess)[i].size; //(processess)[numProcesses - 1].size;
 
 					(holes)[numHoles - 1].start = (processess)[i].start;
 
-					(holes)[numHoles - 1].end = (processess)[i].end - 1;
+					(holes)[numHoles - 1].end = (processess)[i].end; //- 1;
 
-					//if combine: numholes-=1; //if this hole end = any other hole start
-					//index of new hole end = index of old hole? //or jsut travese any hole?
-
-					//combine holes if they next to each other?-only in rl?
+					// if another hole right after it- combine into 1 hole?hole count - 1
 
 //					if ((holes)[numHoles].end == (holes)[numHoles - 1].start) {
 //						//if ((holes)[numHoles - 1].end == (holes)[numHoles].start) { //this hole starts where next hole ends
@@ -188,17 +179,16 @@ int main(int argc, char *argv[]) {
 //						//(holes)[numHoles].start = (holes)[numHoles - 1].start;
 //					}
 
-					//undo some sutff //check for another hole created
+					//change totla sizes
+					totalProcessesSize -= (processess)[i].size; //size;
+					totalHolesSize += (processess)[i].size; //size;
+
+					//empty process
 					strcpy(&(processess)[i].name, "null"); //	&(processess)[numProcesses].name = name;
 					(processess)[i].size = 0;
 
 					(processess)[i].start = 0;
 					(processess)[i].end = 0;
-
-					//change totla sizes
-
-					totalProcessesSize -= (processess)[i].size; //size;
-					totalHolesSize += (processess)[i].size; //size;
 
 					//numProcesses -= 1;
 
@@ -212,18 +202,17 @@ int main(int argc, char *argv[]) {
 		} else if (strcmp(commands, "Status") == 0) {
 
 			/*
-
 			 Status: The Status command used for reporting the status of memory is entered.
 			 */
 
-			printf("Partitions [Allocated memory = %d]:\n", totalProcessesSize); //650000);
+			printf("Partitions [Allocated memory = %d]:\n", totalProcessesSize);
 
 			//loop num processes
-			for (int i = 0; i < numProcesses; i++) { //if name not "null" print? //(strcmp(&(processess)[i].name, "null") == 0)
+			for (int i = 0; i < numProcesses; i++) {
 
 				if (strcmp(&(processess)[i].name, "null") != 0) { //not released
 					printf("Address [%d:%d] Process %s\n",
-							(processess)[i].start, (processess)[i].end,
+							(processess)[i].start, (processess)[i].end - 1,
 							&(processess)[i].name);
 				}
 			}
@@ -231,12 +220,12 @@ int main(int argc, char *argv[]) {
 			printf("\n");
 
 			//loop num holes
-			printf("Holes [Free memory = %d]:\n", totalHolesSize); //350000);
+			printf("Holes [Free memory = %d]:\n", totalHolesSize);
 
 			for (int i = 0; i < numHoles; i++) {
 
 				printf("Address [%d:%d] len = %d\n", (holes)[i].start,
-						(holes)[i].end, (holes)[i].size);
+						(holes)[i].end - 1, (holes)[i].size);
 
 			}
 
